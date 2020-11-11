e14b2ad417499e1242e9a2b93ffafd3ddc596a76
Praneeth Ravichandran <praneeth95@gmail.com>
2020-11-11 13:16:42 +0530

    Code to clone google maps clone

---
+++ script.js
@@ -0,0 +1,32 @@
+mapboxgl.accessToken =
+"pk.eyJ1IjoicHJhbmVldGg5NSIsImEiOiJja2dwOXdjbnUwYTUyMnRsOHRlNGVjb3ZtIn0.vbLWaequMEnDIuCDKGGUbA"
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true
+})
+
+function successLocation(position) {
+  setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+function errorLocation() {
+  setupMap([-2.24, 53.48])
+}
+
+function setupMap(center) {
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: center,
+    zoom: 15
+  })
+
+  const nav = new mapboxgl.NavigationControl()
+  map.addControl(nav)
+
+  var directions = new MapboxDirections({
+    accessToken: mapboxgl.accessToken
+  })
+
+  map.addControl(directions, "top-left")
+}
